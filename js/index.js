4b52a9df24b44bceb0b4d9448468d13a0212a90c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-02 02:18:01 +0300

    Test DOM fully loaded before proceeding

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@
 const slidebtn = document.getElementById("slide");
 const backbtn = document.getElementById("backBtn");
 const nextbtn = document.getElementById("nextBtn");
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
