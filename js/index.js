70f66493531e72c7267e2a78e6668f8885866a12
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-02 02:11:55 +0300

    added variables for slider buttons

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//functionality for back and next button in the image slider
+
+const slidebtn = document.getElementById("slide");
+const backbtn = document.getElementById("backBtn");
+const nextbtn = document.getElementById("nextBtn");
