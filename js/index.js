1f41be3d7a3b60a8d5172fb251fa0388e05945d6
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-02 02:19:31 +0300

    Added array to grab images from gallery

--- js/index.js
+++ js/index.js
@@ -7,3 +7,12 @@ const nextbtn = document.getElementById("nextBtn");
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 });
+
+//Array that will grab the list of images to place in the gallery
+const sliderImg = new Array(
+    "images/img1.jpg",
+    "images/img2.jpg",
+    "images/img3.jpg",
+    "images/img4.jpg",
+
+);
